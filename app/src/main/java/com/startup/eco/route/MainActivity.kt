69e82d233b7c174384e72dafd50050c81b045aed
Muhammad Hakam Fardana <mhakamf13@gmail.com>
2023-10-11 21:11:31 +0700

    Initial commit

---
+++ app/src/main/java/com/startup/eco/route/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.startup.eco.route
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
