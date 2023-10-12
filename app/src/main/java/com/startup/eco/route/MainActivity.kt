cb5724b23bd32e0f67774afb8104b91686b70337
“Mohammad <firmansmoh842@gmail.com>
2023-10-12 17:41:12 +0700

    init project

--- app/src/main/java/com/startup/eco/route/MainActivity.kt
+++ app/src/main/java/com/startup/eco/route/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.startup.eco.route
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.startup.eco.route.ui.Login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            finish()
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        },2000)
     }
 }
---
+++ app/src/main/java/com/startup/eco/route/ui/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.startup.eco.route.ui.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.startup.eco.route.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
