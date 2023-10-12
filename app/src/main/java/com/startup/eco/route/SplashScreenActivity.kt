1d3fe873718003daf79080ea656a7fda604c2669
“Mohammad <firmansmoh842@gmail.com>
2023-10-12 22:07:40 +0700

    add OnBoardingActivity dan Fragments

---
+++ app/src/main/java/com/startup/eco/route/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.startup.eco.route
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.startup.eco.route.ui.onboarding.OnBoardingActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this,R.color.main_green)
+
+        Handler().postDelayed({
+            finish()
+            startActivity(Intent(this@SplashScreenActivity, OnBoardingActivity::class.java))
+        },2000)
+    }
+}
---
+++ app/src/main/java/com/startup/eco/route/ui/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,37 @@
+package com.startup.eco.route.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.startup.eco.route.adapter.ViewPagerAdapter
+import com.startup.eco.route.databinding.ActivityOnBoardingBinding
+import com.startup.eco.route.ui.onboarding.fragments.FistFragment
+import com.startup.eco.route.ui.onboarding.fragments.SecondFragment
+import com.startup.eco.route.ui.onboarding.fragments.ThridFragment
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityOnBoardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentList = arrayListOf<Fragment>(
+            FistFragment(),
+            SecondFragment(),
+            ThridFragment(),
+        )
+
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            this.supportFragmentManager,
+            lifecycle
+        )
+
+        binding.viewPager.adapter = adapter
+        binding.dotsIndicator.attachTo(viewPager2 = binding.viewPager)
+
+    }
+}
